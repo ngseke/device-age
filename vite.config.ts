859920eeb4a3561eee3eaa735bbdc122732b3bd4
ngseke <ngseke@gmail.com>
2022-09-13 00:13:19 +0800

    chore: check TypeScript type with Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslint from 'vite-plugin-eslint'
+import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     eslint(),
+    checker({ typescript: true, vueTsc: true }),
   ],
   base: './',
 })
