9359abe9a55cd8775fafd2b0f65350ccf6aa7d74
ngseke <ngseke@gmail.com>
2022-09-12 22:54:15 +0800

    feat: add title

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,25 @@ import devices from './modules/devices'
 </script>
 
 <template>
-  <main class="container py-4 text-gray-900">
-    <transition-group
-      tag="div"
-      class="flex flex-wrap gap-4 transition-all"
-    >
-      <div
-        v-for="(device) in devices"
-        :key="device.key"
-        class="flex-none"
+  <main class="container flex min-h-screen flex-col justify-center text-gray-900">
+    <div class="p-3">
+      <header class="flex items-center justify-between">
+        <h1 class="mb-5 text-4xl font-bold sm:text-5xl ">
+          Device Age
+        </h1>
+      </header>
+      <transition-group
+        tag="div"
+        class="flex flex-wrap gap-4 transition-all"
       >
-        <Item :device="device" />
-      </div>
-    </transition-group>
+        <div
+          v-for="(device) in devices"
+          :key="device.key"
+          class="flex-none"
+        >
+          <Item :device="device" />
+        </div>
+      </transition-group>
+    </div>
   </main>
 </template>
